0def4ffaaf6c25353903e9938ddaf4d0fa58b687
Dan Hagberg <dan.hagberg@concur.com>
2015-07-17 11:50:26 -0400

    new file to clone bundles from respective repos

---
+++ vimfiles/bundle/loadBundles.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/rizzatti/dash.vim.git
+git clone https://github.com/rizzatti/funcoo.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/OrangeT/vim-csharp.git
